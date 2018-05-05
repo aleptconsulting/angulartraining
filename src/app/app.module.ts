b25d35748faba8d654cf6dc4b6517548fb54bfae
Vishal Avalani <vavalani@affirma.com>
2018-05-05 12:34:33 +0530

    Create new component by ng g c newcomp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { MyComp } from "./mycomp.component";
 import { AppComponent } from "./app.component";
 import { UserData } from "./userdata.service";
 import { MyDepartment } from "./mydepartement.service";
+import { NewcompComponent } from './newcomp/newcomp.component';
 
 @NgModule({
-  declarations: [AppComponent, MyComp],
+  declarations: [AppComponent, MyComp, NewcompComponent],
   imports: [BrowserModule],
   providers: [UserData, MyDepartment],
   bootstrap: [AppComponent, MyComp]
