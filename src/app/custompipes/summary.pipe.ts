ccaed9a5c7eb421864b0be91541d51abca790666
Vishal Avalani <vavalani@affirma.com>
2018-05-05 17:27:24 +0530

    custom pipe modification

--- src/app/custompipes/summary.pipe.ts
+++ src/app/custompipes/summary.pipe.ts
@@ -5,6 +5,6 @@ import { Pipe, PipeTransform } from "@angular/core";
 })
 export class SummaryPipe implements PipeTransform {
   transform(value: string, ...args: any[]) {
-    return value.substring(0, 20) + "...";
+    return value.substring(0, args[0] == null ? 20 : args[0]) + "...";
   }
 }
