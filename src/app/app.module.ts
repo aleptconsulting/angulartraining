8aa2bd1f00db3517f8fcadca555a9fa50cad4d5c
Vishal Avalani <vavalani@affirma.com>
2018-05-05 12:41:14 +0530

    Create header and footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppComponent } from "./app.component";
 import { UserData } from "./userdata.service";
 import { MyDepartment } from "./mydepartement.service";
 import { NewcompComponent } from './newcomp/newcomp.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, MyComp, NewcompComponent],
+  declarations: [AppComponent, MyComp, NewcompComponent, HeaderComponent, FooterComponent],
   imports: [BrowserModule],
   providers: [UserData, MyDepartment],
   bootstrap: [AppComponent, MyComp]
